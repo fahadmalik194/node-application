760496622626c71d385130b0219776bc7de0b587
fahad <muhammad.fahad@techverx.com>
2021-12-07 18:01:20 +0500

    Server Fix

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
-const app = require('express')();
+const express = require('express')
+const app = express()
+const { PORT } = require("../config")
 
-const { PORT } = require("../config");
-const port = process.env.PORT || PORT;
+const port = process.env.PORT || PORT
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from Node Application!!')
+})
+
+app.post('/hello', (req, res) => {
+  res.send(req.body.hello)
 })
 
 app.listen(port, () => console.log(`Server is up on port ${port}`))
